3dc4876fac41b52082ecf0ab5fb8d37317afab0a
HeapHieps <HoHiep1104@gmail.com>
2026-08-28 00:48:58 -0700

    feat: added switch cases for step 5 to onboarding and removed jasmin tests

--- src/app/first-time/onboarding/onboarding.component.ts
+++ src/app/first-time/onboarding/onboarding.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, ChangeDetectionStrategy, input, output, inject, WritableSignal, Signal, signal, computed, Inject, Injector } from '@angular/core';
 import { OnboardingAnimations } from './onboarding.animations';
-import { User } from 'src/app/core/store/user/user.model';
+import { User, OnboardingState } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { OrganizeQuarterlyGoalsComponent } from './step-pages/organize-quarterly-goals/organize-quarterly-goals.component';
+import { DATABASE_SERVICE } from 'src/app/core/firebase/database.service';
 
 @Component({
   selector: 'app-onboarding',
@@ -10,10 +12,14 @@ import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch
   styleUrls: ['./onboarding.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports: [
+    OrganizeQuarterlyGoalsComponent,
+  ],
   animations: OnboardingAnimations,
 })
 export class OnboardingComponent implements OnInit {
   authStore = inject(AuthStore);
+  private db = inject(DATABASE_SERVICE);
   
   // --------------- INPUTS AND OUTPUTS ------------------
 
@@ -22,9 +28,25 @@ export class OnboardingComponent implements OnInit {
   
   // --------------- LOCAL UI STATE ----------------------
 
+  OnboardingState = OnboardingState;
+
   // --------------- COMPUTED DATA -----------------------
 
   // --------------- EVENT HANDLING ----------------------
+
+  async onOrganizeQuarterlyGoalsBack(): Promise<void> {
+    const user = this.currentUser();
+    if (user) {
+      await this.db.updateEntity('users', user.__id, { onboardingState: OnboardingState.STEP_4 });
+    }
+  }
+
+  async onOrganizeQuarterlyGoalsNext(): Promise<void> {
+    const user = this.currentUser();
+    if (user) {
+      await this.db.updateEntity('users', user.__id, { onboardingState: OnboardingState.STEP_6 });
+    }
+  }
 
   // --------------- OTHER -------------------------------
 
